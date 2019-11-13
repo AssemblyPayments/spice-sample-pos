667dbd033f605b7c29663869368c4babdef2d3fc
Raymond Ko <rko@assemblypayments.com>
2019-11-13 13:45:57 +1100

    Zip models

---
+++ spice-sample-pos/spice-sample-pos/Models/ZipPurchaseRequest.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+
+namespace spice_sample_pos.Models
+{
+    public class ZipPurchaseRequest
+    {
+        [JsonProperty(PropertyName = "posRefId")]
+        public string PosRefId { get; set; }
+
+        [JsonProperty(PropertyName = "purchaseAmount")]
+        public int PurchaseAmountCents { get; set; }
+
+        [JsonProperty(PropertyName = "description")]
+        public string Description { get; set; }
+    }
+}
---
+++ spice-sample-pos/spice-sample-pos/Models/ZipRefundRequest.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+
+namespace spice_sample_pos.Models
+{
+    public class ZipRefundRequest
+    {
+        [JsonProperty(PropertyName = "posRefId")]
+        public string PosRefId { get; set; }
+
+        [JsonProperty(PropertyName = "refundAmount")]
+        public int RefundAmountCents { get; set; }
+
+        [JsonProperty(PropertyName = "originalReceiptNumber")]
+        public string ReceiptNumber { get; set; }
+    }
+}
